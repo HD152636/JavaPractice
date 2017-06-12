676a37fa7366b41642e0c92b186d5e8ea6fcc809
허고운 <@DESKTOP-K67G90Q>
2017-06-12 10:04:25 +0900

    fgfg

---
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ *   |_ Music
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 12.
+ * </pre>
+ *
+ * @author      : 허고운
+ * @version     : 1.0
+ */
+public class Music {
+	private String title;
+	private String singer;
+	
+	public Music(String title, String singer){
+		this.title = title;
+		this.singer = singer;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the singer
+	 */
+	public String getSinger() {
+		return singer;
+	}
+
+	/**
+	 * @param singer the singer to set
+	 */
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+	public String toString(){
+		return title + "(" + singer + ")";
+		
+	}
+}
+	
